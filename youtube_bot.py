7f38a888583bb323fbd6b8129bbc395be540e56e
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2021-01-25 03:17:46 -0500

    update

--- youtube_bot.py
+++ youtube_bot.py
@@ -14,14 +14,16 @@ list_browser = []
 browser_1 = webdriver.Chrome(executable_path='./chromedriver')
 browser_2 = webdriver.Chrome(executable_path='./chromedriver')
 browser_3 = webdriver.Chrome(executable_path='./chromedriver')
+browser_4 = webdriver.Chrome(executable_path='./chromedriver')
 
 list_browser.append(browser_1)
 list_browser.append(browser_2)
 list_browser.append(browser_3)
+list_browser.append(browser_4)
 
 for browser in list_browser:
     """ url you want to visit """
-    browser.get('https://www.youtube.com/watch?v=9GQ5AgZqN3Y')
+    browser.get('https://www.youtube.com/watch?v=bp-aIZ7We_g')
 
 while(True):
     number_random = randrange(0, len(list_browser))
