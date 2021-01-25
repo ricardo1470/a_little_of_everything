ffaa52406a7292d1b57998a8cf2b1aa9a4d6d186
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2021-01-25 03:25:48 -0500

    update

--- youtube_bot.py
+++ youtube_bot.py
@@ -14,12 +14,10 @@ list_browser = []
 browser_1 = webdriver.Chrome(executable_path='./chromedriver')
 browser_2 = webdriver.Chrome(executable_path='./chromedriver')
 browser_3 = webdriver.Chrome(executable_path='./chromedriver')
-browser_4 = webdriver.Chrome(executable_path='./chromedriver')
 
 list_browser.append(browser_1)
 list_browser.append(browser_2)
 list_browser.append(browser_3)
-list_browser.append(browser_4)
 
 for browser in list_browser:
     """ url you want to visit """
