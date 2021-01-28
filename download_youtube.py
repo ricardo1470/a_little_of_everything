e4536744ec97b433ff8e27bad021f6b1ad7f4f92
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2021-01-28 02:57:10 -0500

    update

--- download_youtube.py
+++ download_youtube.py
@@ -10,4 +10,4 @@ url = 'https://www.youtube.com/watch?v=7t2o-AS-Kb0&list=RD7t2o-AS-Kb0&start_radi
 youtube = pytube.YouTube(url)
 
 video = youtube.streams.get_by_itag(135)
-video.download('./downloads')
+video.download('C:/Users/cresp/Documents/bots')
